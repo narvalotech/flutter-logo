6045ef91f0746722bd55a982dc1d89db48d07aaf
Jonathan Rico <jonathan@rico.live>
2026-08-09 15:53:08 -0600

    pull messy ble code away from main

--- turtlebot/lib/main.dart
+++ turtlebot/lib/main.dart
@@ -2,9 +2,8 @@ import 'package:flutter/material.dart';
 import 'screens/turn.dart';
 import 'screens/forward.dart';
 import 'screens/back.dart';
-import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
-import 'dart:async';
 import 'dart:convert';
+import 'ble.dart';
 
 void main() {
   runApp(const MyApp());
@@ -175,96 +174,34 @@ class _HomeScreenState extends State<HomeScreen> {
     super.dispose();
   }
 
-  final flutterReactiveBle = FlutterReactiveBle();
-  StreamSubscription<DiscoveredDevice>? _scanSubscription = null;
-  StreamSubscription<ConnectionStateUpdate>? _stateSub = null;
-  bool _connected = false;
-  String? ble_id = null;
+  // ============= ble ==============
 
-  void _connectBle() {
-    if (_stateSub == null && _scanSubscription == null) {
-      _scanSubscription = flutterReactiveBle.scanForDevices(
-        withServices: [], scanMode: ScanMode.lowLatency).listen((device) {
-          if (device.name.contains('turtle')) {
-            print('Found device: ${device}');
-            ble_id = device.id;
-            _scanSubscription?.cancel();
-            _scanSubscription = null;
-
-            // Now let's connect
-            _stateSub = flutterReactiveBle.connectToDevice(
-              id: ble_id!,
-              servicesWithCharacteristicsToDiscover: {},
-              connectionTimeout: const Duration(seconds: 2),
-            ).listen((state) {
-                if (state.connectionState == .connected) {
-                  print('CONNECTED!!');
-                  // now update the MTU
-                  flutterReactiveBle.requestMtu(deviceId: ble_id!, mtu: 500).then((mtu) {
-                      print('Got MTU: ${mtu}');
-                      _connected = true;
-                  });
-                } else if (state.connectionState == .disconnected) {
-                  _connected = false;
-                  ble_id = null;
-                  _stateSub?.cancel();
-                  _stateSub = null;
-                } else {
-                  print('Conn update: ${state}');
-                }
-              }, onError: (error) {
-                print('Conn error: ${error}');
-                _connected = false;
-                _stateSub?.cancel();
-                _stateSub = null;
-            });
-          }
-        }, onError: (e) {
-          print('Error: ${e}');
-      });
-    }
-  }
+  final ble = SimpleBle();
 
   void _sendProgramBle() {
-    _connectBle();
-    if (_connected && ble_id != null) {
-      final serviceUuid = Uuid.parse("6E400001-B5A3-F393-E0A9-E50E24DCCA9E");
-      final characteristicUuid = Uuid.parse("6e400002-b5a3-f393-e0a9-e50e24dcca9e");
+    ble.connect();
 
-      // Write the program "commands" list of TurtleCommand
-      final char = QualifiedCharacteristic(
-        serviceId: serviceUuid,
-        characteristicId: characteristicUuid,
-        deviceId: ble_id!,
-      );
-
-      print('writing: ${char}');
+    if (ble.connected()) {
+      // Write the list of `TurtleCommand`s
       String data = '';
       for (final c in commands) {
         data += '${c.toString()}\n';
       }
-      // Should we clear the instruction list here? or
-      // send a notification to the app somehow.
+
+      // TODO: Should we clear the instruction list here? or send a notification
+      // to the app somehow.
 
       print('data: ${data}');
-      flutterReactiveBle.writeCharacteristicWithoutResponse(
-        char, value: utf8.encode(data)).whenComplete(() {
-          print('LOGO PROGRAM SENT');
-      });
+      ble.send(utf8.encode(data));
     }
   }
 
   void _disconnectBle() {
-    print('Disconnecting');
-    
-    // Disconnect if already connected
-    _stateSub?.cancel();
-    _stateSub = null;
-
-    // Stop any scan in progress
-    _scanSubscription?.cancel();
-    _scanSubscription = null;
+    ble.disconnect();
+    ble.stopScan();
   }
+
+  // ============= ble ==============
 
   void _addCommand(TurtleCommand command) {
     print('Adding: ${command}');
